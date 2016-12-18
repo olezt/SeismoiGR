e4b8e4670fd4ad25d4d14d6a6e76d15deed3cd2f
olezt <lfants666@gmail.com>
2016-12-18 13:24:11 +0200

    Introduce icon styles and refresh functionality

--- www/js/controllers/map.controller.js
+++ www/js/controllers/map.controller.js
@@ -9,12 +9,29 @@ function MapCtrl(MapService, NgMap, $window, $translate, SettingsService) {
 	vm.mapHeight = $window.innerHeight - 92 + "px";
 
 	NgMap.getMap().then(function(map) {
+		loadData(map);
+		var refreshDiv = document.createElement('div');
+        var centerControl = new CenterControl(refreshDiv, map);
+
+        refreshDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(refreshDiv);
+	});
+	
+	
+	function loadData(map) {
 		map.data.loadGeoJson(calculateApiUrl(), null, function() {
 			createInfoWindows(map);
 			setIconStyle(map);
 		});
-	});
+	}
 
+	function clearData(map) {
+		map.data.forEach(function(feature) {
+		    map.data.remove(feature);
+		});
+		google.maps.event.clearListeners(map.data, 'click');
+	}
+	
 	function calculateApiUrl() {
 		var startTime = calculateTimeRequest();
 		var apiUrl = 'http://www.seismicportal.eu/fdsnws/event/1/query?limit=1000&start=' + startTime + '&minlat=33.853&maxlat=41.707&minlon=18.578&maxlon=27.901&minmag=' + SettingsService.getRange() + '&format=json';
@@ -47,14 +64,58 @@ function MapCtrl(MapService, NgMap, $window, $translate, SettingsService) {
 
 	function setIconStyle(map) {
 		map.data.setStyle(function(feature) {
-			var mag = feature.getProperty('mag');
+			var magnitude = feature.getProperty('mag');
+			var color;
+			if(magnitude<3){
+				color = 'blue';
+			}else if(magnitude<4){
+				color = 'yellow';
+			}else {
+				color = 'red';
+			}
 			return {
 				icon : {
-					path : google.maps.SymbolPath.CIRCLE,
-					scale : mag + 2
+					path: google.maps.SymbolPath.CIRCLE,
+			        fillColor: color,
+			        fillOpacity: .5,
+			        scale: Math.pow(2, magnitude) / 1.7,
+			        strokeColor: 'white',
+			        strokeWeight: .5
 				}
 			};
 		});
 	}
+	
+    function CenterControl(controlDiv, map) {
 
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.style.marginRight = '10px';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '25px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = '<i class = "icon icon ion-refresh"></i>';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: simply set the map to Chicago.
+        controlUI.addEventListener('click', function() {
+        	clearData(map);
+        	loadData(map);
+        });
+
+      }
 }
