1c79e37e659dac962a369a4b7349ab307ea20c0e
olezt <lfants666@gmail.com>
2016-12-28 02:43:37 +0200

    Disable window 3d animations

--- www/js/controllers/map.controller.js
+++ www/js/controllers/map.controller.js
@@ -14,6 +14,7 @@ function MapCtrl(MapService, NgMap, $window, $translate, SettingsService, $scope
 	var blinkInterval;
 	var recent = [];
 	var rectangle;
+	window.L_DISABLE_3D = true;
 	
 	if(ConnectionService.getConnection()){
 		vm.isOnline = true;
