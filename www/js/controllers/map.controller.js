a0299f8e94a776dbc2dbd62f6c802f08a1a7df83
olezt <lfants666@gmail.com>
2017-01-28 19:01:20 +0200

    Switch cors to my heroku app
    -https://murmuring-tundra-79115.herokuapp.com/

--- www/js/controllers/map.controller.js
+++ www/js/controllers/map.controller.js
@@ -72,9 +72,9 @@ function MapCtrl(MapService, NgMap, $window, $translate, SettingsService, $scope
 		var apiUrl;
 		if(!init && MapService.getDynamicBounds()){
 			var dynamicBounds = JSON.parse(MapService.getDynamicBounds());
-			apiUrl = 'https://cors-anywhere.herokuapp.com/http://www.seismicportal.eu/fdsnws/event/1/query?limit='+SETTINGS.MARKERS_LIMIT+'&start=' + startTime + '&minlat='+dynamicBounds.south+'&maxlat='+dynamicBounds.north+'&minlon='+dynamicBounds.west+'&maxlon='+dynamicBounds.east+'&minmag=' + SettingsService.getRange() + '&format=json';
+			apiUrl = 'https://murmuring-tundra-79115.herokuapp.com/http://www.seismicportal.eu/fdsnws/event/1/query?limit='+SETTINGS.MARKERS_LIMIT+'&start=' + startTime + '&minlat='+dynamicBounds.south+'&maxlat='+dynamicBounds.north+'&minlon='+dynamicBounds.west+'&maxlon='+dynamicBounds.east+'&minmag=' + SettingsService.getRange() + '&format=json';
 		}else{
-			apiUrl = 'https://cors-anywhere.herokuapp.com/http://www.seismicportal.eu/fdsnws/event/1/query?limit='+SETTINGS.MARKERS_LIMIT+'&start=' + startTime + '&minlat='+STATIC_BOUNDS.SOUTH+'&maxlat='+STATIC_BOUNDS.NORTH+'&minlon='+STATIC_BOUNDS.WEST+'&maxlon='+STATIC_BOUNDS.EAST+'&minmag=' + SettingsService.getRange() + '&format=json';
+			apiUrl = 'https://murmuring-tundra-79115.herokuapp.com/http://www.seismicportal.eu/fdsnws/event/1/query?limit='+SETTINGS.MARKERS_LIMIT+'&start=' + startTime + '&minlat='+STATIC_BOUNDS.SOUTH+'&maxlat='+STATIC_BOUNDS.NORTH+'&minlon='+STATIC_BOUNDS.WEST+'&maxlon='+STATIC_BOUNDS.EAST+'&minmag=' + SettingsService.getRange() + '&format=json';
 		}
 		return apiUrl;
 	}
